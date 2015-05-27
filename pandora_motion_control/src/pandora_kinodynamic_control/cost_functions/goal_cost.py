532e6b7f62b073ec386e944c826217d5fca56ed5
Christos Tsirigotis <tsirif@gmail.com>
2015-05-27 20:24:34 +0300

    Add GoalCost implementation of CostNode to cost the pose error

---
+++ pandora_motion_control/src/pandora_kinodynamic_control/cost_functions/goal_cost.py
@@ -0,0 +1,68 @@
+from cost_graph.cost_node import CostNode
+from geometry_msgs.msg import Pose
+from tf import transformations
+import math
+
+def find_distance(pose_a, pose_b):
+    quaternion = (
+        pose_a.orientation.x,
+        pose_a.orientation.y,
+        pose_a.orientation.z,
+        pose_a.orientation.w)
+    euler = transformations.euler_from_quaternion(quaternion)
+    yaw_a = euler[2]
+    quaternion = (
+        pose_b.orientation.x,
+        pose_b.orientation.y,
+        pose_b.orientation.z,
+        pose_b.orientation.w)
+    euler = transformations.euler_from_quaternion(quaternion)
+    yaw_b = euler[2]
+
+    x_diff = pose_a.position.x - pose_b.position.x
+    y_diff = pose_a.position.y - pose_b.position.y
+    yaw_diff = yaw_a - yaw_b
+
+    distance = math.sqrt(x_diff**2 + y_diff**2 + yaw_diff**2)
+    return distance
+
+class GoalCost(CostNode):
+
+    """Implementation of CostNode for calculating a cost from goal
+       based errors"""
+
+    def __init__(self):
+        """Initiates a GoalCost object"""
+        super(GoalCost, self).__init__()
+        self.expected_pose = Pose()
+        self.actual_pose = Pose()
+
+    def update_cost(self):
+        """Uses expected final pose (goal) and actual final pose in order to
+        calculate a cost. Updates self.__cost
+        @return: nothing
+
+        """
+        self.__cost = find_distance(self.expected_pose, self.actual_pose)
+
+    def set_goal_pose(self, pose):
+        """Sets goal pose, expected pose of vehicle when movement command will
+        have finished
+
+        @param pose Pose, expected final vehicle's pose
+        (we care about x, y and yaw)
+        @return: nothing
+
+        """
+        self.expected_pose = pose
+
+    def set_actual_pose(self, pose):
+        """Sets final pose, actual pose of vehicle when movement command has
+        finished
+
+        @param pose Pose, actual final vehicle's pose
+        (we care about x, y and yaw)
+        @return: nothing
+
+        """
+        self.actual_pose = pose
