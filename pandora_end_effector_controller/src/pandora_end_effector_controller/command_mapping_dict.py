2918910a622281298c82a8fd9a2b063ae994bac7
geromidg <geromidg@gmail.com>
2015-07-22 04:11:06 +0800

    Change camera effector from soft_move to move and adjust sensor orientation params

--- pandora_end_effector_controller/src/pandora_end_effector_controller/command_mapping_dict.py
+++ pandora_end_effector_controller/src/pandora_end_effector_controller/command_mapping_dict.py
@@ -62,6 +62,6 @@ translate_command = {
         MoveEndEffectorGoal.PARK: MoveSensorGoal.CENTER,
         MoveEndEffectorGoal.TRACK: MoveSensorGoal.POINT,
         MoveEndEffectorGoal.LAX_TRACK: MoveSensorGoal.LAX_POINT,
-        MoveEndEffectorGoal.SCAN: MoveSensorGoal.SOFT_MOVE
+        MoveEndEffectorGoal.SCAN: MoveSensorGoal.MOVE
     }
 }
